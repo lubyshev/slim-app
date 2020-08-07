9693aa18a58da792470a279f886b6aa16824237e
lubyshev <demagog@demagog.demagog>
2020-08-07 13:02:23 +0300

    Add dotenv package

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
 <?php
+declare(strict_types=1);
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
+
+(Dotenv\Dotenv::createImmutable(dirname(__DIR__)))->load();
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
+
     return $response;
 });
 
